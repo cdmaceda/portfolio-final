f4b3f03f929fe8e899d0388072e9237990b3e5a9
cdmaceda <cdmaceda@yahoo.com>
2022-07-20 23:43:10 -0300

    creacion de componentes header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/header/logo-ap/logo-ap.component';
+import { LogoRedesComponent } from './components/header/logo-redes/logo-redes.component';
+import { BannerComponent } from './components/header/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    LogoRedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
