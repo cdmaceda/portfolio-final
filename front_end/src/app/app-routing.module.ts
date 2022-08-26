5e844136feba3f4e0a63e8ca6cc3b9acd3218709
cdmaceda <cdmaceda@yahoo.com>
2022-08-26 12:30:01 -0300

    termine el componente Educacion, quedo terminado y funcionando Estudios y Experiencia laboral

--- front_end/src/app/app-routing.module.ts
+++ front_end/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditEducacionComponent } from './components/educacion/edit-educacion/edit-educacion.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion/new-educacion.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,7 +14,9 @@ const routes: Routes = [
   //{path:'login', component: LoginComponent} lo comente porque cambie una ruta
   {path:'login', component: LoginPageComponent},
   {path:'nuevaexp', component: NewExperienciaComponent},
-  {path:'editexp/:id', component: EditExperienciaComponent}
+  {path:'editexp/:id', component: EditExperienciaComponent},
+  {path:'nuevaedu', component: NewEducacionComponent},
+  {path:'editedu/:id', component: EditEducacionComponent}
 ];
 
 @NgModule({
--- front_end/src/app/components/experiencia/edit-experiencia/edit-experiencia.component.ts
+++ front_end/src/app/components/experiencia/edit-experiencia/edit-experiencia.component.ts
@@ -26,6 +26,7 @@ export class EditExperienciaComponent implements OnInit {
   onUpdate(): void{
     const id = this.activatedRouter.snapshot.params['id'];
     this.sExperiencia.update(id, this.expLab).subscribe(data =>{
+      alert("Experiencia modificada correctamente");
       this.router.navigate(['']);
     }, err =>{
       alert("Error al modificar experiencia");
